734f00d7141a947eced924b41aec817e79b2bc89
PavloKom <pakamar4@gmail.com>
2025-12-11 21:40:16 +0200

    Delete HomeTask2.py

--- HomeTask2.py
+++
@@ -1,4 +0,0 @@
-FirstNumber = float(input("Enter First number: "))
-SecondNumber = float(input("Enter Second number:"))
-ThirdNumber = float(input("Enter Third number:"))
-print("Average: ", ((FirstNumber + SecondNumber + ThirdNumber) / 3 ))
