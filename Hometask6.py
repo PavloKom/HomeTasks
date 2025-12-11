5288786b04bbd2e7e5588f85176460c3131ac44c
PavloKom <pakamar4@gmail.com>
2025-12-11 21:39:38 +0200

    Delete Hometask6.py

--- Hometask6.py
+++
@@ -1,4 +0,0 @@
-lenght = float(input("Enter lenght: "))
-widght = float(input("Enter widght: "))
-print("Аrea of a rectangle: ", lenght * widght)
-print("Perimeter of a rectangle: ", 2 * (lenght + widght))
