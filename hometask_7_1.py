fd0458119052a5a3f9f5ca66b54020c86ea91fb9
PavloKom <pakamar4@gmail.com>
2026-01-05 19:01:08 +0200

    Add files via upload

---
+++ hometask_7_1.py
@@ -0,0 +1,7 @@
+name = input("enter your name: ")
+age =int(input("enter your age: "))
+
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old."
+
+print(say_hi(name, age))
