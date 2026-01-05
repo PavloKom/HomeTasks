5e559792faf79f2d237468ec7be86a3b83182ed4
PavloKom <pakamar4@gmail.com>
2026-01-05 22:01:08 +0200

    Delete Hometask_7_4.py

--- Hometask_7_4.py
+++
@@ -1,8 +0,0 @@
-def common_elements():
-    multiples_3 = [x for x in range(100) if x % 3 == 0]
-    multiples_5 = [y for y in range(100) if y % 5 == 0]
-
-    return set(multiples_3) & set(multiples_5)
-
-
-print(common_elements())
