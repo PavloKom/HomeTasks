1a9665febb4654ec147f8d36f6133269a1da452b
PavloKom <pakamar4@gmail.com>
2026-01-05 21:54:39 +0200

    Delete hometask_7_1.py

--- hometask_7_1.py
+++
@@ -1,7 +0,0 @@
-name = input("enter your name: ")
-age =int(input("enter your age: "))
-
-def say_hi(name, age):
-    return f"Hi. My name is {name} and I'm {age} years old."
-
-print(say_hi(name, age))
