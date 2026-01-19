e76da17a3984091e2a7ef34e896e200c12055a0d
PavloKom <pakamar4@gmail.com>
2026-01-19 15:45:59 +0200

    Add files via upload

---
+++ hometask_11_2.py
@@ -0,0 +1,18 @@
+def generate_cube_numbers(end):
+    num = 2
+    while True:
+        cube = num ** 3
+        if cube >= end:
+            break
+        yield cube
+        num += 1
+
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True
+assert list(generate_cube_numbers(10)) == [8]
+assert list(generate_cube_numbers(100)) == [8, 27, 64]
+print("ok")
+print(list(generate_cube_numbers(1000)))
